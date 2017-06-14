239a0538ceda1c98da5390bdd77bc16650ca6ebf
Jose <josepablo2692@gmail.com>
2017-06-14 14:29:14 -0600

    Primera parte de Reedit Clone

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Reedit Clone</title>
+
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+  </head>
+  <body>
+        <div class="container">
+          <div class="row">
+            <div class="col-md-12">
+              <h1>Reedit Clone</h1>
+            </div>
+          </div>
+
+          <hr>
+          @yield('content')
+        </div>
+
+  </body>
+</html>
